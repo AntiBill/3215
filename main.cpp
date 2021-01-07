b6c534c6cf3ffd448767b10a7121c86109ecfa23
AntiBill <46397366+AntiBill@users.noreply.github.com>
2021-01-07 16:52:59 +0000

    reading files and printing their contents now works

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 //Real-Time Coursework
 
+#include <fstream>
 #include <iostream>
+using namespace std;
 
 int main() {
-    // prints the string enclosed in double quotes
-    std::cout << "This is C++ Programming";
+    int sum = 0;
+    int x;
+    char num[1];
+    ifstream inFile;
+
+    inFile.open("input.txt");
+    
+    if (!inFile) {
+        cerr << "Unable to open file datafile.txt";
+        exit(1);   // call system to stop
+    }
+
+    while (inFile >> x) {
+        sum = sum + x;
+    }
+    
+    // prints the contents of the file
+    cout << sum << endl;
+    cin >> num[0];
     return 0;
 }
