7c9628067894b854ad7a224012158fa9364870f0
AntiBill <46397366+AntiBill@users.noreply.github.com>
2021-01-08 11:23:39 +0000

    Made numTask recognition efficient

--- main.cpp
+++ main.cpp
@@ -2,27 +2,40 @@
 
 #include <fstream>
 #include <iostream>
+#include <string> 
 using namespace std;
 
 int main() {
-    int sum = 0;
-    int x;
-    char num[1];
+    int counter = 0;
+    int numTasks = 0;
+    char task[5];
+    string t;
+
     ifstream inFile;
 
     inFile.open("input.txt");
     
     if (!inFile) {
         cerr << "Unable to open file datafile.txt";
-        exit(1);   // call system to stop
+        exit(1);   // call system to stop with an error
     }
 
-    while (inFile >> x) {
-        sum = sum + x;
+    //Read the first 2 strings of the file. Ignore the string 'NumTasks' and convert the number of tasks to an integer
+    while (inFile >> t && counter < 2) {
+        if(counter == 1) {
+            numTasks = stoi(t);
+        }
+        counter++;
     }
+
+    //At this point, numTasks = the number of tasks
+    for(int i = 0; i < numTasks; i++) {
+
+    }
+
     
     // prints the contents of the file
-    cout << sum << endl;
-    cin >> num[0];
+    cout << numTasks << endl;
+    cin >> t;
     return 0;
 }
